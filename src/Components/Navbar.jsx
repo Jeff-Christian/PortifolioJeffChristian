79ed69c66756d56b714489668b04b782a41508a9
Jefferson Christian <jeffchristian.webdesigner@gmail.com>
2025-05-10 15:20:17 -0300

    firsts frontend ajusts

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <section className="relative-nav">
+        <div>
+          <nav>
+            <ul>
+              <li>Sobre Mim</li>
+              <li>Projetos</li>
+              <li>Algo</li>
+              <li>Contato</li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <h1>logo</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Navbar;
