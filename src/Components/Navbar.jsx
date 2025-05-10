9cfa325fcd5a697c629a6243a0dcd0d24829b64d
Jefferson Christian <jeffchristian.webdesigner@gmail.com>
2025-05-10 17:55:19 -0300

    creating section navbar with slogan elements

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,44 @@
 import React from "react";
+import "../CSS/Navbar.css";
+import logo from "../assets/CREATIVECHRISTIAN.svg";
 
 function Navbar() {
   return (
     <>
       <section className="relative-nav">
-        <div>
+        <div className="nav">
           <nav>
             <ul>
-              <li>Sobre Mim</li>
-              <li>Projetos</li>
-              <li>Algo</li>
-              <li>Contato</li>
+              <li>
+                <a href="/">Sobre Mim</a>
+              </li>
+              <li>
+                <a href="/work">Projetos</a>
+              </li>
+              <li>
+                <a href="/news">Algo</a>
+              </li>
+              <li>
+                <a href="/contact">Contato</a>
+              </li>
             </ul>
           </nav>
         </div>
         <div>
-          <h1>logo</h1>
+          <img className="logo" type="image/svg+xml" src={logo} alt="" />
+        </div>
+      </section>
+      <section className="nav-header">
+        <div className="nav-header-subtitle">
+          <h2 className="name">
+            <span className="name_jeff">Jéfferson</span>
+            <span className="name_christian">Christian</span>
+          </h2>
+        </div>
+        <div className="nav-header-slogan">
+          <h1>
+            Design e código <span>com identidade</span>
+          </h1>
         </div>
       </section>
     </>
