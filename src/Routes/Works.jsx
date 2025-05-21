d58b32fefa668056f5a245d8518873e0ef100f1e
Jefferson Christian <jeffchristian.webdesigner@gmail.com>
2025-05-21 17:47:19 -0300

    started project page

--- src/Routes/Works.jsx
+++ src/Routes/Works.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 
+import "../CSS/Works.css";
+
 function Works() {
   return (
     <>
       <Navbar />
+      <div className="worksContainer">
+        <h2>Projetos Recentes</h2>
+        <p>
+          <p className="textWorks">Works. </p> Explore uma coleção dos meus
+          projetos mais recentes de branding, conteúdo e produto desenvolvidos
+          para diferentes marcas.
+        </p>
+      </div>
       <Footer />
     </>
   );
