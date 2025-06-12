3efa5503a1cfe4d04161f4b9186fcdcb72901ecf
Jefferson Christian <jeffchristian.webdesigner@gmail.com>
2025-06-12 13:50:33 -0300

    inserting projects in page with respective images, ajust in responsive breakpoints

--- src/Routes/Works.jsx
+++ src/Routes/Works.jsx
@@ -4,6 +4,14 @@ import Footer from "../Components/Footer";
 
 import "../CSS/Works.css";
 
+import projeto1 from "../assets/Weseeyou.png";
+import projeto2 from "../assets/Essën.png";
+import projeto3 from "../assets/Emócta studio.png";
+import projeto4 from "../assets/Owl Blog.png";
+import projeto5 from "../assets/Anime Tracker.png";
+import projeto6 from "../assets/Sentir.png";
+import projeto7 from "../assets/Além do mapa.png";
+
 function Works() {
   return (
     <>
@@ -15,6 +23,122 @@ function Works() {
           projetos mais recentes de branding, conteúdo e produto desenvolvidos
           para diferentes marcas.
         </p>
+        <div className="projects">
+          <a href="">
+            <div className="top">
+              <div className="left">
+                <p>We See You</p>
+                <p className="projectType">Website</p>
+              </div>
+              <div className="right">
+                <p className="project-number">01</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="frame">
+                <img src={projeto1} alt="" />
+              </div>
+            </div>
+          </a>
+          <a href="">
+            <div className="top">
+              <div className="left">
+                <p>Essën</p>
+                <p className="projectType">Eccomerce</p>
+              </div>
+              <div className="right">
+                <p className="project-number">02</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="frame">
+                <img src={projeto2} alt="" />
+              </div>
+            </div>
+          </a>
+          <a href="">
+            <div className="top">
+              <div className="left">
+                <p>Emócta studio</p>
+                <p className="projectType">Website Studio</p>
+              </div>
+              <div className="right">
+                <p className="project-number">03</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="frame">
+                <img src={projeto3} alt="" />
+              </div>
+            </div>
+          </a>
+          <a href="">
+            <div className="top">
+              <div className="left">
+                <p>Owl Blog</p>
+                <p className="projectType">Blog</p>
+              </div>
+              <div className="right">
+                <p className="project-number">04</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="frame">
+                <img src={projeto4} alt="" />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className="projects">
+          <a href="">
+            <div className="top">
+              <div className="left">
+                <p>Anime Tracker List</p>
+                <p className="projectType">Crud</p>
+              </div>
+              <div className="right">
+                <p className="project-number">05</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="frame">
+                <img src={projeto5} alt="" />
+              </div>
+            </div>
+          </a>
+          <a href="">
+            <div className="top">
+              <div className="left">
+                <p>Sentir</p>
+                <p className="projectType">App Meditação</p>
+              </div>
+              <div className="right">
+                <p className="project-number">06</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="frame">
+                <img src={projeto6} alt="" />
+              </div>
+            </div>
+          </a>
+          <a href="">
+            <div className="top">
+              <div className="left">
+                <p>Além do Mapa</p>
+                <p className="projectType">Website</p>
+              </div>
+              <div className="right">
+                <p className="project-number">07</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="frame">
+                <img src={projeto7} alt="" />
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
       <Footer />
     </>
